3faf91af0de5d228e56c5c88d0fa891efaca1f66
Ziykon-Haykon <127660730+Ziykon-Haykon@users.noreply.github.com>
2024-11-04 21:45:09 +0500

    Add files via upload

---
+++ project/scripts/scripts.js
@@ -0,0 +1,57 @@
+// Функции всякие
+
+function toggleMenu() {
+    const menu = document.getElementById("dropdownMenu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+function openLogin() {
+    document.getElementById("loginModal").style.display = "flex";
+    document.getElementById("dropdownMenu").style.display = "none";
+}
+
+function openRegister() {
+    document.getElementById("registerModal").style.display = "flex";
+    document.getElementById("dropdownMenu").style.display = "none";
+}
+
+function closeLogin() {
+    document.getElementById("loginModal").style.display = "none";
+}
+
+function closeRegister() {
+    document.getElementById("registerModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    const loginModal = document.getElementById("loginModal");
+    const registerModal = document.getElementById("registerModal");
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
+    if (event.target == registerModal) {
+        registerModal.style.display = "none";
+    }
+};
+
+function validatePasswords() {
+    const password = document.getElementById("register-password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const errorMessage = document.getElementById("error-message");
+
+    if (password !== confirmPassword) {
+        errorMessage.textContent = "Пароли не совпадают.";
+        return false;
+    }
+
+    errorMessage.textContent = "";
+    return true;
+}
+
+ function connectToWiFi() {
+    alert("Функция в разработке")
+ }
+
+
+
+// Как заставить ислама работать нормально, а то ля меня не устраивает то что он "Поиск информации". Ничего не делает, если и делает то это помощь Диме, он сам говорил то что ислам это его моральная поддержка. Ля бесит, даже Егор с Макаром будут умнее Ислама с Димой.
